c6f074b4ef068ed3078263de28f5298be77abb59
Furrydan <danannj@gmail.com>
2026-09-02 12:08:10 -0400

    Changed node version for github actions

--- novelot-frontend/src/test/setup.ts
+++ novelot-frontend/src/test/setup.ts
@@ -1,10 +1,1 @@
 import '@testing-library/jest-dom/vitest'
-import { beforeAll, afterAll, vi } from 'vitest'
-
-beforeAll(() => {
-    vi.spyOn(console, 'error').mockImplementation(() => {})
-})
-
-afterAll(() => {
-    vi.restoreAllMocks()
-})
